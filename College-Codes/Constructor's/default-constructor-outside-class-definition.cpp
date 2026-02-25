e122916fd83853a16b7bad215584dd472def9cd3
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-02-25 20:50:50 +0530

    Create default-constructor-outside-class-definition.cpp

---
+++ College-Codes/Constructor's/default-constructor-outside-class-definition.cpp
@@ -0,0 +1,36 @@
+// Default Constructor Outside Class Definition.cpp
+
+#include<iostream>
+using namespace std;
+
+class company{
+  string name;
+  int profit;
+  
+  public:
+  void display();
+  
+  //Default Constructor.
+  company(){
+      name = "Himesh";
+      profit = 150000;
+  }
+};
+  
+  void company::display(){       //Outside Class Definition.
+      cout << "Name = " << name << endl;
+      cout << "Profit = " << profit << endl;
+  }
+
+int main(){
+    company s1;
+    s1.display();
+    
+    
+    return 0;
+}
+
+/*
+Name = Himesh
+Profit = 150000
+*/
