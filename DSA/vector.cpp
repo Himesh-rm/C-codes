c7a2d4e75b8c1645e72a78c897f6d2ee3cc33b6a
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-03-17 19:31:33 +0530

    Create vector.cpp

---
+++ DSA/vector.cpp
@@ -0,0 +1,39 @@
+// size,push_back,pop_back,front,back,at.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> vec = {25,63,12,45,78};
+    
+    cout << "Before Push_back size of vector is: " << vec.size() << endl;
+    
+    vec.push_back(6);                                                        // Add 6 to the given vector.
+    cout << "After Push_back size of vector is: " << vec.size() << endl;
+    
+    vec.pop_back();                                                          // Remove last index in given vector.
+    
+    cout << "First Element of vector is: " << vec.front() << endl;           // To print first index value of vector.
+    
+    cout << "Last Element of vector is: " << vec.back() << endl;             // To print last index value of vector.
+    
+    cout << "Printing value using 'at' function: " << vec.at(0) << endl;
+    
+    cout << "Printing Value's of vector using For-Each_Loop: " << endl;
+  
+    for(int x: vec){      // For-Each Loop.
+        cout << x << " ";
+    }
+    return 0;
+}
+
+/*
+Before Push_back size of vector is: 5
+After Push_back size of vector is: 6
+First Element of vector is: 25
+Last Element of vector is: 78
+Printing value using 'at' function: 25
+Printing Value's of vector using For-Each_Loop: 
+25 63 12 45 78 
+*/
