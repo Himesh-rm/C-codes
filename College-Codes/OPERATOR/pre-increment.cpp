46013db1d0c66071512d202e1c6659b4ff9fa4bb
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-04-02 07:09:00 +0530

    Create pre-increment.cpp

---
+++ College-Codes/OPERATOR/pre-increment.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class abc{
+  int m,n;
+  
+ public:
+  abc(){
+      m = 8;
+      n = 9;
+  }
+  
+  void operator ++(){
+      ++m;
+      ++n;
+  }
+  
+  void show(){
+      cout << "m: " << m << "\nn: " << n << endl;
+  }
+};
+
+int main(){
+    abc x;
+    x.show();
+    ++x;
+    x.show();
+    
+    return 0;
+}
+
+/*
+m: 8
+n: 9
+m: 9
+n: 10
+*/
