0e81358efb2d26ed109e489089ccf935d032d6fc
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-04-12 08:09:40 +0530

    Create virtual function.cpp

---
+++ College-Codes/Imp-Codes/virtual function.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class A{
+public:
+    virtual void display(){
+        cout << "THIS IS CLASS A." << endl;
+    }
+};
+
+class B:public A{
+public:
+     void display(){ 
+        cout << "THIS IS CLASS B." << endl;
+    }
+};
+
+class C:public A{
+public:
+     void display(){
+        cout << "THIS IS CLASS C." << endl;
+    }
+};
+
+int main(){
+    A *Himesh;
+    B Rudra;
+    C Jenil;
+    
+    Himesh = &Rudra;
+    Himesh->display();
+    
+    Himesh = &Jenil;
+    Himesh->display();
+    
+    return 0;
+}
+
+/*
+THIS IS CLASS B.
+THIS IS CLASS C.
+*/
