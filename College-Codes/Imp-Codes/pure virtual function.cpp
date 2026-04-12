6f6ed253ce15f424267c94577873fa7ac3641f58
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-04-12 08:41:30 +0530

    Create pure virtual function.cpp

---
+++ College-Codes/Imp-Codes/pure virtual function.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class A{
+public:
+    virtual void display() = 0;
+};
+
+class B:public A{
+public:
+     void display() override{ 
+        cout << "THIS IS CLASS B." << endl;
+    }
+};
+
+class C:public A{
+public:
+     void display() override{
+        cout << "THIS IS CLASS C." << endl;
+    }
+};
+
+int main(){
+    A *Himesh;
+    B Rudra;
+    C Jenil;
+    
+    Himesh = &Rudra;
+    Himesh->display();
+    
+    Himesh = &Jenil;
+    Himesh->display();
+    
+    return 0;
+}
+
+/*
+THIS IS CLASS B.
+THIS IS CLASS C.
+*/
